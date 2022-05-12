101cbace4762e933eccdc16ad4830f6189221ff8
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2022-05-12 11:20:29 -0400

    add file

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # author: LM (AMDG) 5/3/22
 # file manager for student hours
 # we ask user to input student's information
-while True:
+question = True
+while question == True
     def student_info():
+        dictionary_for_students_hours = {}
         name = input("Please enter the student's name: ")
-        address = input("Please enter the student's address:")
-        phone = input("Please enter Parent's phone number:")
+        address = input("Please enter the student's address: ")
+        phone = input("Please enter Parent's phone number: ")
+        hours = float(input("Please enter the student's number of hours completed: "))
         with open('students_service_hours.txt', 'a') as infile:
             infile.write('Name: {0}'.format(name))
             infile.write('\n')
@@ -13,17 +16,19 @@ while True:
             infile.write('\n')
             infile.write("Parent's Phone Number: {0}".format(phone))
             infile.write("\n")
-            infile.write("Student's hours: ")
+            infile.write("Student's hours: {0}".format(hours))
             infile.write("\n")
             infile.write("\n")
         return name, address, phone
 
 
     def repeat_student_info():
+        # call gliobal variable
         while True:
             anotherstudent = input("Will you be adding another student? Enter 'Y' if yes or 'N' if no: ").capitalize()
             if anotherstudent == 'Y':
                 print("You will be adding another student...")
+                question = True
             elif anotherstudent == 'N':
                 print("Thank you. Have a good day.")
                 break
@@ -31,6 +36,6 @@ while True:
                 print("Enter a 'Y' or 'N'")
                 continue
             return
-    
+
     student_info()
-    repeat_student_info()
+    repeat_student_info()  
